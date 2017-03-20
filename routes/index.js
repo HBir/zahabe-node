d0b7c0bdaeb2533b82c46eb4b8401593ab3845c6
Hannes Birgersson <hannesbirgersson@gmail.com>
2017-03-20 23:32:45 +0100

    Added sqlite promises. Made daily random from daily seed

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,23 @@
-var express = require('express');
+import express from 'express';
 var app = express();
-var Promise = require("bluebird");
-var sqlite3  = require("sqlite3");
+import Promise from 'bluebird';
+// var sqlite3  = require("sqlite3");
+import db from 'sqlite';
 
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
 	
+  
+
 	getAllMVs(function(mvs) {
-		console.log(mvs)
+    var rng = new RNG(getDate());
+    let daily = rng.nextRange(1,mvs.length)
+    console.log(mvs[daily])
 		res.render('index', { 
 			title: 'Minns vi den gången Zahabe', 
-			MVs: mvs
+			MVs: mvs,
+      daily: mvs[daily]
 		});
 	});
 
@@ -21,17 +27,53 @@ app.get('/', function(req, res, next) {
 
 module.exports = app;
 
+Promise.resolve()
+  .then(() => db.open('./db/zahabe.sqlite', { Promise }))
+  .catch(err => console.error(err.stack));
+  // .finally(() => app.listen(port));
 
 function getAllMVs(callback) {
-	let db = new sqlite3.Database("zahabe.sqlite");
-	let mvs;
-	db.serialize(() => {
-	    db.all("SELECT * FROM MinnsDu ORDER BY MVOrder desc", function(err, rows) {
-	        mvs = rows;
-	        callback(mvs);
-	    });
-	});
+  Promise.all([
+    db.all(`SELECT Text, ID, Story, SkrivenAv, (select count(*) from MinnsDu b  where a.id >= b.id) as cnt
+            FROM MinnsDu a LEFT JOIN Stories ON a.ID = Stories.MVID ORDER BY MVOrder desc`, {Promise})
+  ]).then(function([mvs]){
+    callback(mvs);
+  });
+}
 
-	db.close();
-	
+function getDate() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    return year + month+ day
+}
+
+function RNG(seed) {
+  // LCG using GCC's constants
+  this.m = 0x80000000; // 2**31;
+  this.a = 1103515245;
+  this.c = 12345;
+
+  this.state = seed ? seed : Math.floor(Math.random() * (this.m-1));
+}
+RNG.prototype.nextInt = function() {
+  this.state = (this.a * this.state + this.c) % this.m;
+  return this.state;
+}
+RNG.prototype.nextFloat = function() {
+  // returns in range [0,1]
+  return this.nextInt() / (this.m - 1);
+}
+RNG.prototype.nextRange = function(start, end) {
+  // returns in range [start, end): including start, excluding end
+  // can't modulu nextInt because of weak randomness in lower bits
+  var rangeSize = end - start;
+  var randomUnder1 = this.nextInt() / this.m;
+  return start + Math.floor(randomUnder1 * rangeSize);
+}
+RNG.prototype.choice = function(array) {
+  return array[this.nextRange(0, array.length)];
 }
